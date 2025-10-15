9c1a1b62e009a40f0e73acdea49f80324b0bf097
tyulyukov <makstyulyukov@gmail.com>
2025-10-15 21:02:00 +0300

    feat: Initial server setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World! The server is running.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
